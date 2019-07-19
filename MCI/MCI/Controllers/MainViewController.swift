b4cfda56fe8a3bf08c406088200024b774a36659
Galdineris <galdineris@gmail.com>
2019-07-19 14:35:18 -0300

    Bottom Sheet Prototype Working

---
+++ MCI/MCI/Controllers/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MCI
+//
+//  Created by Rafael Galdino on 18/07/19.
+//  Copyright © 2019 Rafael Galdino. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addBottomSheetView()
+    }
+    
+    func addBottomSheetView(){
+        let bottomSheetVC = BottomSheetViewController()
+        
+        self.addChild(bottomSheetVC)
+        self.view.addSubview(bottomSheetVC.view)
+        bottomSheetVC.didMove(toParent: self)
+        
+        let height = view.frame.height
+        let width = view.frame.width
+        bottomSheetVC.view.frame = CGRect(x: 0, y: self.view.frame.maxY, width: width, height: height)
+    }
+
+}
